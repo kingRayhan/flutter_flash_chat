c54d5454ed7bb631998f4868f127199d0ed82863
KingRayhan <rayhan095@gmail.com>
2023-10-07 10:32:29 +0600

    firebase integration and form validation

--- lib/common/widgets/app_textfield.dart
+++ lib/common/widgets/app_textfield.dart
@@ -1,13 +1,23 @@
 import 'package:flash_chat/common/constants.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AppTextfield extends StatelessWidget {
   AppTextfield(
-      {Key? key, this.controller, this.onChanged, this.hintText = "Hint Text"})
+      {Key? key,
+      this.controller,
+      this.onChanged,
+      this.hintText = "Hint Text",
+      this.obscureText = false,
+      this.keyboardType,
+      this.validator})
       : super(key: key);
 
   Function(String?)? onChanged;
   String? hintText;
+  bool? obscureText;
+  TextInputType? keyboardType;
+  String? Function(String?)? validator;
   TextEditingController? controller = TextEditingController();
 
   @override
@@ -15,6 +25,9 @@ class AppTextfield extends StatelessWidget {
     return TextFormField(
       controller: controller,
       onChanged: onChanged,
+      obscureText: obscureText!,
+      keyboardType: keyboardType,
+      validator: validator,
       decoration: InputDecoration(
         hintText: hintText,
         constraints: const BoxConstraints(minHeight: 22),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/common/widgets/app_button.dart';
 import 'package:flash_chat/common/widgets/app_textfield.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
@@ -13,6 +16,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _firebaseAuth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,13 +38,34 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ),
             ),
             const SizedBox(height: 48.0),
-            AppTextfield(),
-            const SizedBox(height: 8.0),
-            AppTextfield(),
-            const SizedBox(height: 24.0),
-            AppButton(
-              label: "Register",
-              onPressed: () {},
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  AppTextfield(
+                    hintText: "Email Address",
+                    keyboardType: TextInputType.emailAddress,
+                    validator:
+                        ValidationBuilder().email().maxLength(50).build(),
+                  ),
+                  const SizedBox(height: 8.0),
+                  AppTextfield(
+                    hintText: "Password",
+                    obscureText: true,
+                    validator: ValidationBuilder()
+                        .required()
+                        .minLength(6)
+                        .maxLength(20)
+                        .build(),
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  const SizedBox(height: 24.0),
+                  AppButton(
+                    label: "Register",
+                    onPressed: _handleRegisterUser,
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 12.0),
             _bottomText(context)
@@ -46,6 +73,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         ),
       ),
     );
+  }
+
+  void _handleRegisterUser() async {
+    _formKey.currentState!.validate();
+    print("Validating user input");
+    // try {
+    //   final newUser = await _firebaseAuth.createUserWithEmailAndPassword(
+    //       email: "rayhan.dev.bd@gmail.com", password: "rayhan123");
+    //   print(newUser.user!.uid);
+    // } catch (e) {
+    //   print(e.toString());
+    // }
   }
 
   RichText _bottomText(BuildContext context) {
